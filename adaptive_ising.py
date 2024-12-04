d89ad6bdc32cad8b60c3724ba8342900360fd876
NickJUhlhorn <nick.uhlhorn@gmail.com>
2024-12-04 15:38:44 -0800

    spacing

--- adaptive_ising.py
+++ adaptive_ising.py
@@ -33,13 +33,16 @@ class Simulation:
         '''update of the spin n according to the heat bath method'''
         news = 0
         pp = 1 / (1 + (1 / np.exp(2 * self.beta * (self.m + self.H))))
+
         if (self.casual() < pp):
             news = 1
         else:
             news = -1
+
         if (news != self.s[n]):
             self.s[n] = -self.s[n]
             self.m += 2.0 * self.s[n] / self.N
+
         self.H -= self.c * self.m * self.dt
 
     def update(self):
