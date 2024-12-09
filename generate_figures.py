181a8c2d2ca6dbd1372c6ecc5b82b6ebec8d430b
NickJUhlhorn <nick.uhlhorn@gmail.com>
2024-12-09 11:23:45 -0800

    1d?

--- generate_figures.py
+++ generate_figures.py
@@ -8,16 +8,21 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import KernelDensity
 
 bin_count = 10000
-save_path = 'figure_c1.png'
+save_path_1c = 'figure_1c.png'
+save_path_1d = 'figure_1d.png'
 data_paths = ['simulation_output.txt']
 
 if len(sys.argv) > 1:
     bin_count = int(sys.argv[1])
 if len(sys.argv) > 2:
-    save_path = sys.argv[2]
+    save_path_1c = sys.argv[2]
 if len(sys.argv) > 3:
-    data_paths = sys.argv[3:]
-print(f"bin_count:{bin_count} | save_path:{save_path}\ndata_paths:{data_paths}")
+    save_path_1d = sys.argv[3]
+if len(sys.argv) > 4:
+    data_paths = sys.argv[4:]
+print(f"bin_count:{bin_count} | save_path_1c:{save_path_1c} | save_path_1d:{save_path_1d}\ndata_paths:{data_paths}")
+print(save_path_1d[-3:])
+assert not (save_path_1c[-3:] == 'txt') and not (save_path_1d[-3:] == 'txt'), 'DO NOT SAVE OVER TEXT FILES (SAFTEY FOR DATA)'
 
 m_lists = []
 
@@ -41,6 +46,7 @@ for data_path in data_paths:
 # a_0 is area under m(t) between two zero-crossing events
 # P(a_0) is the distrabution of the zero-crossing area
 a_0_lists = []
+t_lists = []
 
 for i, m_list in enumerate(m_lists):
     signs = np.sign(m_list)
@@ -48,6 +54,7 @@ for i, m_list in enumerate(m_lists):
     signs_offset[-1] = signs[-1]
 
     zero_crossings = np.append([0], np.where(signs != signs_offset))
+    ts = np.diff(zero_crossings)
 
     print(f"Summing Areas Under Curve for list {i+1}")
     a_0s = []
@@ -67,11 +74,11 @@ for i, m_list in enumerate(m_lists):
     a_0s = np.array(a_0s)
     a_0s = a_0s[np.where(a_0s >= 0)]
     a_0_lists.append(a_0s)
+    t_lists.append(ts)
 
 p_of_a_0s_lists = []
-
 for i, a_0s in enumerate(a_0_lists):
-    print(f'Building Distribution {i+1}')
+    print(f'Building a_0 Distribution {i+1}')
 
     # samples = a_0s[np.random.choice(a_0s.shape[0], 1000, replace=False)]
 
@@ -89,10 +96,29 @@ for i, a_0s in enumerate(a_0_lists):
     p_of_a_0s = hist_probabilities[bin_indexes]
     p_of_a_0s_lists.append(p_of_a_0s)
 
+p_of_t_lists = []
+for i, ts in enumerate(t_lists):
+    print(f'Building t Distribution {i+1}')
+    # samples = a_0s[np.random.choice(a_0s.shape[0], 1000, replace=False)]
+
+    # kde = KernelDensity(kernel='gaussian', bandwidth=0.2, algorithm='kd_tree', atol=1e-3, rtol=1e-2).fit(samples.reshape(-1,1))
+    # print("Scoring values")
+    # p_of_a_0s = np.exp(kde.score_samples(a_0s.reshape(-1,1)))
+
+    hist, bin_edges = np.histogram(ts, bin_count)
+    bin_edges[0] -= 1
+    bin_edges[-1] += 1
+
+    bin_indexes = np.subtract(np.digitize(ts, bin_edges, True), 1)
+    hist_probabilities = np.divide(hist, len(ts))
+
+    p_of_t = hist_probabilities[bin_indexes]
+    p_of_t_lists.append(p_of_t)
+
 params = {'mathtext.default': 'regular' }          
 plt.rcParams.update(params)
 
-print(f"Saving Figure as {save_path}")
+print(f"Saving Figure as {save_path_1c}")
 plt.figure(figsize=(6,4.5))
 
 plt.yscale('log')
@@ -107,8 +133,29 @@ for i in range(len(a_0_lists)):
 
 plt.xlabel('$a_0$')
 plt.ylabel('$P(a_0)$')
-leg = plt.legend(framealpha=1, fontsize=12, markerscale=2.5)
+leg = plt.legend(framealpha=1, fontsize=12, markerscale=2.5, loc='upper right')
 for lh in leg.legend_handles: 
     lh.set_alpha(1)
 
-plt.savefig(save_path)
+plt.savefig(save_path_1c)
+
+print(f"Saving Figure as {save_path_1d}")
+plt.figure(figsize=(6,4.5))
+
+plt.yscale('log')
+plt.xscale('log')
+plt.xlim(0.1, 20000)
+plt.ylim(0.00001, 1)
+
+labels = [r'$\beta=1$', r'$\beta=0.99$', r'$\beta=0.9$', r'$\beta=0.7$']
+colors = ['black', 'red', 'blue', 'yellow']
+for i in range(len(a_0_lists)):
+    plt.scatter(a_0_lists[i], p_of_a_0s_lists[i], s=0.1, alpha=0.25, label=labels[i], c=colors[i])
+
+plt.xlabel('$t$')
+plt.ylabel('$P(t)$')
+leg = plt.legend(framealpha=1, fontsize=12, markerscale=2.5, loc='upper right')
+for lh in leg.legend_handles: 
+    lh.set_alpha(1)
+
+plt.savefig(save_path_1d)
